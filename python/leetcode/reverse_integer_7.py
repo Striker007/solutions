5fc2bbe149c16ca7da607083472367ee59b2a59d
Nikita Voloboev <nikita.voloboev@gmail.com>
2016-12-11 15:03:28 +0100

    solved 7

---
+++ python/leetcode/reverse_integer_7.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            return int(str(x)[1:][::-1])*-1
+        # convert number to string, reverse the string, then convert it back to integer
+        else:
+            return int(str(x)[::-1])
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.reverse(-123))
+    str = "hello"
+    print(str[1:])
