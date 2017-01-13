84fa7082619145051494a1e57895ed1f30915e56
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-01-13 02:48:41 +0100

    two sum done

---
+++ python/leetcode/palindrome_number_9.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    # constraint : do it with no extra space
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPalindrome(101))
+
